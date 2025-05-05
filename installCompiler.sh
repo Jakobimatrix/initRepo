982e0b724bca7e62e0baf0519144794e4bd4bf48
Jakob Wandel <Sonnentau.koenig@gmail.com>
2025-05-05 21:41:50 +0200

    add missing sudo

--- installCompiler.sh
+++ installCompiler.sh
@@ -8,8 +8,8 @@ GCC_VERSION="13"
 read -p "Do you want to install Clang ${CLANG_VERSION}? [y/N] " install_clang
 if [[ "$install_clang" =~ ^[Yy]$ ]]; then
     echo "Installing Clang ${CLANG_VERSION}..."
-    wget -qO- https://apt.llvm.org/llvm.sh | bash -s -- "$CLANG_VERSION"
-    apt install -y clang-${CLANG_VERSION} lld-${CLANG_VERSION} lldb-${CLANG_VERSION}
+    wget -qO- https://apt.llvm.org/llvm.sh | sudo bash -s -- "$CLANG_VERSION"
+    sudo apt install -y clang-${CLANG_VERSION} lld-${CLANG_VERSION} lldb-${CLANG_VERSION}
     clang-$CLANG_VERSION --version
 else
     echo "Skipping Clang installation."
@@ -18,10 +18,10 @@ fi
 read -p "Do you want to install GCC ${GCC_VERSION}? [y/N] " install_gcc
 if [[ "$install_gcc" =~ ^[Yy]$ ]]; then
     echo "Adding GCC PPA and installing GCC ${GCC_VERSION}..."
-    add-apt-repository -y ppa:ubuntu-toolchain-r/test
-    apt update
-    apt install -y gcc-${GCC_VERSION} g++-${GCC_VERSION} gdb
-    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${GCC_VERSION} 60 --slave /usr/bin/g++ g++ /usr/bin/g++-${GCC_VERSION}
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt update
+    sudo apt install -y gcc-${GCC_VERSION} g++-${GCC_VERSION} gdb
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${GCC_VERSION} 60 --slave /usr/bin/g++ g++ /usr/bin/g++-${GCC_VERSION}
     gcc-${GCC_VERSION} --version
 else
     echo "Skipping GCC installation."
