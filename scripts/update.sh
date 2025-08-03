c720cd86fc4c2ab192283659ecfc36e8b217c5c2
Jakob Wandel <Sonnentau.koenig@gmail.com>
2025-08-03 22:16:05 +0200

    simple update script

---
+++ scripts/update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# update.sh
+# Updates the initRepo subrepository and optionally updates default files in the main repo.
+
+set -e
+
+# Go to the directory of this script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+
+
+git stash
+git pull
+
+# Go to the main repository root
+cd ../../
+
+git add initRepo
+
+# Ask user if they want to update default files
+read -p "Update default files? [y/n]: " yn
+case $yn in
+    [Yy]* )
+        echo "Running ./initRepo/scripts/init.sh ..."
+        ./initRepo/scripts/init.sh
+        ;;
+    * )
+        echo "Default files not updated."
+        ;;
+esac
