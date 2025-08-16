addc3c0a936812ed11fe691777474f919d3a8a25
Jakob Wandel <Sonnentau.koenig@gmail.com>
2025-08-16 22:55:18 +0200

    coverage working?

--- scripts/showCoverage.sh
+++ scripts/showCoverage.sh
@@ -24,5 +24,5 @@ fi
 
 ./initRepo/scripts/build.sh -c -d -g -t -T --compiler gcc
 
-gcovr --gcov-executable gcov-${GCC_VERSION} -r . --exclude '.*_deps/.*' --gcov-ignore-parse-errors > coverage.txt
-less coverage.txt
+gcovr --gcov-executable gcov-${GCC_VERSION} -r . build-gcc-${GCC_VERSION}-debug/src --exclude '.*_deps/.*' --gcov-ignore-parse-errors > coverage.txt 2>/dev/null
+cat coverage.txt
